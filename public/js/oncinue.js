6da0ea9ce21a2e606bdfeb6764e3b31a971d5a38
Grayback_Gen <shalelord6192@gmail.com>
2017-11-20 09:40:43 +0800

    Refresh

--- public/js/oncinue.js
+++
@@ -1,9 +0,0 @@
-// <script source="jquery-3.2.1.min.js" type="text/javascript"></script>
-
-// $(document).ready(function() {
-// 	$('#useroption').val('active');
-
-// });
-$(document).ready(function(){
-	$('#useroption').val('active');
-});
