835f9a811a4418d5ced59f49c3666911103d4a8d
Grayback_Gen <shalelord6192@gmail.com>
2017-12-14 20:43:24 +0800

    Revert "Doctrack 12-14-17"
    This reverts commit 57e544fa82b9c4d18c54f9a09e544eb97c7fb94f.

---
+++ public/js/oncinue.js
@@ -0,0 +1,9 @@
+// <script source="jquery-3.2.1.min.js" type="text/javascript"></script>
+
+// $(document).ready(function() {
+// 	$('#useroption').val('active');
+
+// });
+$(document).ready(function(){
+	$('#useroption').val('active');
+});
